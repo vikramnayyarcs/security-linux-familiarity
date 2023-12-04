bd2d559eb177d1f9efdf9068271351dec9443f7b
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-12-04 21:08:54 +0000

    Update request.py

--- CSC3632-21-kchal-6S68CSLE/request.py
+++ CSC3632-21-kchal-6S68CSLE/request.py
@@ -18,3 +18,22 @@ print(response2.text)
 
 # Optionally, you can clear cookies after the second request if needed
 session.cookies.clear()
+
+session2 = requests.Session() 
+
+# Define the URLs
+url3 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideMonthDoB=on&hideLastDigitZIP=on"
+
+# Make the first request and store cookies in the session
+response3 = session.get(url1)
+
+# Make the second request using the same session
+response4 = session.get(url3)
+
+# Print the content of the second response
+print(response4.text)
+
+# Optionally, you can clear cookies after the second request if needed
+session.cookies.clear()
+
+
