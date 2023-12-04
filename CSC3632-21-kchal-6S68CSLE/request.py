2fec55fa836a5bf1c2c2d520c1def04a9bf6839a
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-12-04 22:11:21 +0000

    Update request.py

--- CSC3632-21-kchal-6S68CSLE/request.py
+++ CSC3632-21-kchal-6S68CSLE/request.py
@@ -1,24 +1,24 @@
 import requests
 
-while True:
-    # Make a session object
-    session = requests.Session()
+# Make a session object
+session = requests.Session()
 
-    # Define the URLs
-    url1 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
-    url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastTwoDigitZIP=on"
+# Define the URLs
+url1 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
+url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastTwoDigitZIP=on"
 
-    # Make the first request and store cookies in the session
-    response1 = session.get(url1)
+# Make the first request and store cookies in the session
+response1 = session.get(url1)
 
-    # Make the second request using the same session
-    response2 = session.get(url2)
+# Make the second request using the same session
+response2 = session.get(url2)
 
-    # Print the content of the second response
-    print(response2.text)
+# Print the content of the second response
+print(response2.text)
 
-    # Optionally, you can clear cookies after the second request if needed
-    session.cookies.clear()
+# Optionally, you can clear cookies after the second request if needed
+session.cookies.clear()
+    
 
 
 
