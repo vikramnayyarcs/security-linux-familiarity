5b34f3af088a382e41584987d0d9324c11733ffd
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-12-04 22:34:52 +0000

    New script.

--- CSC3632-21-kchal-6S68CSLE/request-while.py
+++ CSC3632-21-kchal-6S68CSLE/request-while.py
@@ -1,30 +1,35 @@
 import requests
 
-# Create a session object outside the loop
-session = requests.Session()
+# Define the URLs
+url1 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
+url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastThreeDigitZIP=on"
+url3 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/PKHGDNHMXH.php?solution=%27DoB,ZIP%20code,Gender,Disease\n30/08/1970,12156,M,Angina%20Pectoris%20\n17/08/1960,12154,F,Stroke\n23/08/1970,12153,M,High%20Cholesterol%20\n07/08/1970,12141,F,High%20Cholesterol%20\n20/09/1970,12143,F,Cardiomyopathy%20\n02/08/1960,12153,F,Stroke\n10/09/1960,12158,F,Angina%20Pectoris%20\n12/09/1960,12159,F,Cardiomyopathy\n02/09/1960,12147,F,Hepatitis\n04/09/1970,12152,M,Hepatitis%20\n10/08/1960,12140,F,Stroke\n05/09/1960,12145,M,Flu\n12/09/1970,12148,F,Eczema%20\n05/09/1970,12155,M,Pneumonia%20\n20/08/1960,12141,F,Stroke\n05/08/1970,12142,F,Erythema%27"
 
 while True:
-    try:
-        # Define the URLs
-        url1 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
-        url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastTwoDigitZIP=on"
+    # Make a session object
+    session = requests.Session()
 
-        # Make the first request and store cookies in the session
-        response1 = session.get(url1)
 
-        # Make the second request using the same session
-        response2 = session.get(url2)
+    # Make the first request and store cookies in the session
+    response1 = session.get(url1)
 
-        # Print the content of the second response
-        # Commenting out the print for potential performance improvement
-        # print(response2.text)
+    # Make the second request using the same session
+    response2 = session.get(url2)
 
-    except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
+    # Print the content of the second response
+    print(response2.text)
 
-    finally:
-        # Optionally, you can clear cookies after the second request if needed
+    response3 = session.get(url3)
+    if "flag" in response3.text:
+        print(response3.text)
         session.cookies.clear()
+        break
+    else:
+        print("NO FLAG")
 
-# Close the session outside the loop
-session.close()
+    # Optionally, you can clear cookies after the second request if needed
+    session.cookies.clear()
+    
+
+
+
--- CSC3632-21-kchal-6S68CSLE/request.py
+++ CSC3632-21-kchal-6S68CSLE/request.py
@@ -6,6 +6,7 @@ session = requests.Session()
 # Define the URLs
 url1 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
 url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastThreeDigitZIP=on"
+url3 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/PKHGDNHMXH.php?solution=%27DoB,ZIP%20code,Gender,Disease\n30/08/1970,12156,M,Angina%20Pectoris%20\n17/08/1960,12154,F,Stroke\n23/08/1970,12153,M,High%20Cholesterol%20\n07/08/1970,12141,F,High%20Cholesterol%20\n20/09/1970,12143,F,Cardiomyopathy%20\n02/08/1960,12153,F,Stroke\n10/09/1960,12158,F,Angina%20Pectoris%20\n12/09/1960,12159,F,Cardiomyopathy\n02/09/1960,12147,F,Hepatitis\n04/09/1970,12152,M,Hepatitis%20\n10/08/1960,12140,F,Stroke\n05/09/1960,12145,M,Flu\n12/09/1970,12148,F,Eczema%20\n05/09/1970,12155,M,Pneumonia%20\n20/08/1960,12141,F,Stroke\n05/08/1970,12142,F,Erythema%27"
 
 # Make the first request and store cookies in the session
 response1 = session.get(url1)
@@ -16,7 +17,7 @@ response2 = session.get(url2)
 # Print the content of the second response
 print(response2.text)
 
-response3 = session.get("http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/PKHGDNHMXH.php?solution=%27DoB,ZIP%20code,Gender,Disease\n30/08/1970,12156,M,Angina%20Pectoris%20\n17/08/1960,12154,F,Stroke\n23/08/1970,12153,M,High%20Cholesterol%20\n07/08/1970,12141,F,High%20Cholesterol%20\n20/09/1970,12143,F,Cardiomyopathy%20\n02/08/1960,12153,F,Stroke\n10/09/1960,12158,F,Angina%20Pectoris%20\n12/09/1960,12159,F,Cardiomyopathy\n02/09/1960,12147,F,Hepatitis\n04/09/1970,12152,M,Hepatitis%20\n10/08/1960,12140,F,Stroke\n05/09/1960,12145,M,Flu\n12/09/1970,12148,F,Eczema%20\n05/09/1970,12155,M,Pneumonia%20\n20/08/1960,12141,F,Stroke\n05/08/1970,12142,F,Erythema%27")
+response3 = session.get(url3)
 print("**RESPONSE 3**")
 print(response3.text)
 
