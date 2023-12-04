f4ae3ba5452415b2eef3d770b2f60bd1fd1a3dd7
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-12-04 14:39:10 +0000

    Rewrite.

--- CSC3632-21-kchal-6S68CSLE/rewritten.py
+++ CSC3632-21-kchal-6S68CSLE/rewritten.py
@@ -4,28 +4,29 @@ from urllib.parse import urlencode
 from itertools import product
 from concurrent.futures import ThreadPoolExecutor
 
-#All in 1 second.
+while True:
+    base_url = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
 
-#Go to the first page.
-base_url = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
-response = requests.get(base_url)
+    #Make the initial request.
+    response_1 = requests.get(base_url)
+    soup_1 = BeautifulSoup(response_1.text, 'html.parser')
 
-soup_1 = BeautifulSoup(response.text, 'html.parser')
+    k_anonymity_element = soup_1.find('p', text=lambda text: 'k-anonymity equal to:' in text)
 
-k_anonymity_element = soup_1.find('p', text=lambda text: 'k-anonymity equal to:' in text)
+    response_1.cookies.clear()
+    response_1.close()
 
-k_anonymity_target = 0
+    #Work out what we need to work towards.
+    if k_anonymity_element:
+        k_anonymity_target = int(''.join(filter(str.isdigit, k_anonymity_element.text)))
 
-#Scrape the desired k anonymity.
-if k_anonymity_element:
-    # Extract the K-anonymity value from the element's text
-    k_anonymity_target = int(''.join(filter(str.isdigit, k_anonymity_element.text)))
+        if k_anonymity_target == 4:
+            url_2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastThreeDigitZIP=on"
 
-#Use one of the hardcoded results here:
-hardcoded_ks = {
-    # 1: "",
-    2: "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastThreeDigitZIP=on",
-    # 3: "",
-    4: "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastThreeDigitZIP=on&hideGender=on",
-    # 5: "",
-}
+            response_2 = requests.get(url_2)
+
+            print(response_2.text)
+
+            response_2.cookies.clear()
+            response_2.close()
+
