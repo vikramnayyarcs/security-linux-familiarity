6af2bb38023fcf0f5bec297feacda3aaf3962345
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-12-04 20:53:36 +0000

    Cleanse scripts.

--- CSC3632-21-kchal-6S68CSLE/script4.py
+++
@@ -1,9 +0,0 @@
-#Just get the target:
-
-
-
-
-#Create a dictionary for each possibility.
-
-
-#Send the appropriate request.
---
+++ CSC3632-21-kchal-6S68CSLE/test.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+
+# then make a url variable
+url1 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
+url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastTwoDigitZIP=on"
+
+# then call the default open method described above
+webbrowser.open(url1)
+webbrowser.open(url2)
