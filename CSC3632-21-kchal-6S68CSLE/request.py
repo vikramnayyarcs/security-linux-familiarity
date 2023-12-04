8d9f1d18c961d90b90d9a51246bbbd4d09968605
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-12-04 22:59:34 +0000

    Update request.py

--- CSC3632-21-kchal-6S68CSLE/request.py
+++ CSC3632-21-kchal-6S68CSLE/request.py
@@ -5,7 +5,7 @@ session = requests.Session()
 
 # Define the URLs
 url1 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
-url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastThreeDigitZIP=on&hideYearDoB=on"
+url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideGender=on&hideDayDoB=on&hideLastTwoDigitZIP=on&hideLastFourDigitZIP=on&hideLastFiveDigitZIP=on"
 url3 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/PKHGDNHMXH.php?solution=%27DoB,ZIP%20code,Gender,Disease\n30/08/1970,12156,M,Angina%20Pectoris%20\n17/08/1960,12154,F,Stroke\n23/08/1970,12153,M,High%20Cholesterol%20\n07/08/1970,12141,F,High%20Cholesterol%20\n20/09/1970,12143,F,Cardiomyopathy%20\n02/08/1960,12153,F,Stroke\n10/09/1960,12158,F,Angina%20Pectoris%20\n12/09/1960,12159,F,Cardiomyopathy\n02/09/1960,12147,F,Hepatitis\n04/09/1970,12152,M,Hepatitis%20\n10/08/1960,12140,F,Stroke\n05/09/1960,12145,M,Flu\n12/09/1970,12148,F,Eczema%20\n05/09/1970,12155,M,Pneumonia%20\n20/08/1960,12141,F,Stroke\n05/08/1970,12142,F,Erythema%27"
 
 # Make the first request and store cookies in the session
