939e587c1f77bfb23abf21d810bfa5b484289d59
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-12-04 10:43:55 +0000

    Log file.

--- CSC3632-21-kchal-6S68CSLE/script2.py
+++ CSC3632-21-kchal-6S68CSLE/script2.py
@@ -5,16 +5,16 @@ from itertools import product
 from concurrent.futures import ThreadPoolExecutor
 
 # Function to make a request and check K-anonymity
-def check_k_anonymity(params):
+def check_k_anonymity(params, k_anonymity_target):
     base_url = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php"
     full_url = f"{base_url}?{urlencode(params)}"
     response = requests.get(full_url)
 
-    # Log the attempt
-    print(f"Attempted URL: {full_url}")
-    print(f"Response: {response.status_code}")
-    print(f"Content: {response.text}")
-    print()
+    # Log the attempt to a file
+    with open('log.txt', 'a') as log_file:
+        log_file.write(f"Attempted URL: {full_url}\n")
+        log_file.write(f"Response: {response.status_code}\n")
+        log_file.write(f"Content: {response.text}\n\n")
 
     # Check if 'flag' is in the response text
     if 'flag' in response.text:
@@ -33,12 +33,11 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 # Find the K-anonymity value directly from the <p> element
 k_anonymity_element = soup.find('p', text=lambda text: 'k-anonymity equal to:' in text)
-print(f"TARGET: {k_anonymity_target}")
 
 # Check if k_anonymity_element is not None before accessing its attributes
 if k_anonymity_element:
     # Extract the K-anonymity value from the element's text
-    k_anonymity = int(''.join(filter(str.isdigit, k_anonymity_element.text)))
+    k_anonymity_target = int(''.join(filter(str.isdigit, k_anonymity_element.text)))
 
     # List of options
     options = [
@@ -59,11 +58,11 @@ if k_anonymity_element:
         combinations = list(product(['on', ''], repeat=len(options)))
 
         # Submit tasks to the executor
-        executor.map(check_k_anonymity, [{option: state for option, state in zip(options, combo)} for combo in combinations])
+        executor.map(check_k_anonymity, [{option: state for option, state in zip(options, combo)} for combo in combinations], [k_anonymity_target]*len(combinations))
 
     # Close the initial webpage
     response.close()
 
-    print(f"No successful attempt for K-anonymity {k_anonymity}. Retrying...")
+    print(f"No successful attempt for K-anonymity {k_anonymity_target}. Retrying...")
 else:
     print("K-anonymity element not found. Check the HTML structure.")
