8bbba0195431beba58fbf8253ae5d6b5c4f66b1f
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-12-04 21:34:44 +0000

    Tweak.

--- CSC3632-21-kchal-6S68CSLE/request.py
+++ CSC3632-21-kchal-6S68CSLE/request.py
@@ -1,23 +1,29 @@
 import requests
 
-# Make a session object
-session = requests.Session()
+while True:
+    # Make a session object
+    session = requests.Session()
 
-# Define the URLs
-url1 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
-url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastTwoDigitZIP=on"
+    # Define the URLs
+    url1 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
+    url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastTwoDigitZIP=on"
 
-# Make the first request and store cookies in the session
-response1 = session.get(url1)
+    # Make the first request and store cookies in the session
+    response1 = session.get(url1)
 
-# Make the second request using the same session
-response2 = session.get(url2)
+    # Make the second request using the same session
+    response2 = session.get(url2)
 
-# Print the content of the second response
-print(response2.text)
+    # Print the content of the second response
+    if "solution" in response2.text:
+        print(response2.text)
+        session.cookies.clear()
+        break 
+    else:
+        print("GOING AGAIN")
 
-# Optionally, you can clear cookies after the second request if needed
-session.cookies.clear()
+    # Optionally, you can clear cookies after the second request if needed
+    session.cookies.clear()
 
 
 
