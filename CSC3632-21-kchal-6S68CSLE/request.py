90d51fe81dc979e2779e7154a1521d1863c96f0f
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-12-04 23:59:15 +0000

    Update request.py

--- CSC3632-21-kchal-6S68CSLE/request.py
+++ CSC3632-21-kchal-6S68CSLE/request.py
@@ -6,8 +6,7 @@ session = requests.Session()
 # Define the URLs
 url1 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
 url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideLastTwoDigitZIP=on"
-url3 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/PKHGDNHMXH.php?solution=4"
-# url3 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/PKHGDNHMXH.php?solution=%27DoB,ZIP%20code,Gender,Disease\n30/08/1970,12156,M,Angina%20Pectoris%20\n17/08/1960,12154,F,Stroke\n23/08/1970,12153,M,High%20Cholesterol%20\n07/08/1970,12141,F,High%20Cholesterol%20\n20/09/1970,12143,F,Cardiomyopathy%20\n02/08/1960,12153,F,Stroke\n10/09/1960,12158,F,Angina%20Pectoris%20\n12/09/1960,12159,F,Cardiomyopathy\n02/09/1960,12147,F,Hepatitis\n04/09/1970,12152,M,Hepatitis%20\n10/08/1960,12140,F,Stroke\n05/09/1960,12145,M,Flu\n12/09/1970,12148,F,Eczema%20\n05/09/1970,12155,M,Pneumonia%20\n20/08/1960,12141,F,Stroke\n05/08/1970,12142,F,Erythema%27"
+url3 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/PKHGDNHMXH.php?solution='DoB,ZIP code,Gender,Disease\n30/08/1970,94156,M,Angina Pectoris \n05/09/1970,94155,M,Pneumonia \n02/09/1970,94152,M,Hepatitis \n20/09/1970,94143,F,Cardiomyopathy \n10/09/1960,94158,F,Angina Pectoris \n05/09/1960,94145,M,Flu\n07/08/1970,94141,F,High Cholesterol \n02/09/1960,94147,M,Hepatitis\n20/08/1960,94141,M,Stroke\n12/09/1970,94148,F,Eczema \n05/08/1970,94142,F,Erythema \n10/08/1960,94140,M,Stroke\n02/08/1960,94153,F,Stroke\n30/09/1960,94159,F,Cardiomyopathy\n25/08/1970,94153,M,High Cholesterol \n01/08/1960,94154,F,Stroke'"
 
 # Make the first request and store cookies in the session
 response1 = session.get(url1)
