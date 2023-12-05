710a9ae0cec47899b03970064235c209722c51a3
Vikram Nayyar <66060875+vncomp@users.noreply.github.com>
2023-12-05 10:47:46 +0000

    Update request-open.py

--- CSC3632-21-kchal-6S68CSLE/request-open.py
+++ CSC3632-21-kchal-6S68CSLE/request-open.py
@@ -6,6 +6,7 @@ br = mechanize.Browser()
 # Define the URLs
 url1 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JGWPPWTCHR.php"
 url2 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/JTDIFDVIUX.php?hideDayDoB=on&hideYearDoB=on&hideLastDigitZIP=on&hideLastThreeDigitZIP=on&hideLastFourDigitZIP=on&hideLastFiveDigitZIP=on"
+#Use functions for URL 3
 url3 = "http://10.0.0.5/ctf_deploy2/kchal/Clyhbjgi/PKHGDNHMXH.php?solution=%27DoB,ZIP%20code,Gender,Disease\n**/09/****,*****,M,Flu\n**/09/****,*****,F,Angina%20Pectoris%20\n**/08/****,*****,F,Stroke\n**/08/****,*****,F,Erythema%20\n**/08/****,*****,M,Angina%20Pectoris%20\n**/08/****,*****,M,Stroke\n**/08/****,*****,M,High%20Cholesterol%20\n**/09/****,*****,F,Cardiomyopathy\n**/09/****,*****,F,Cardiomyopathy%20\n**/08/****,*****,M,Stroke\n**/09/****,*****,F,Eczema%20\n**/09/****,*****,M,Pneumonia%20\n**/08/****,*****,F,High%20Cholesterol%20\n**/08/****,*****,F,Stroke\n**/09/****,*****,M,Hepatitis\n**/09/****,*****,M,Hepatitis%27"
 
 # Make the first request
